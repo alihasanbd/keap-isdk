0832659f95ecfabed76cee73ea84e5b82cba5d60
Ali Hasan <alihasan.iubat@gmail.com>
2022-04-17 00:27:52 +0600

    Copied files

---
+++ sample/connection-test.php
@@ -0,0 +1,12 @@
+<?php
+require_once(__DIR__ . '/../src/isdk.php');
+
+$app = new iSDK;
+echo "created object!<br/>";
+
+if ($app->cfgCon("connectionName", "")) {
+
+  echo "app connected!<br/>";
+} else {
+  echo "connection failed!<br/>";
+}
